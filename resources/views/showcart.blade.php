f8040ba365ba07ce4b02dd6826739795bcbaf397
Mohammed Khalef <eyeschico@mohammedkhalef.com>
2021-10-19 22:11:07 +0200

    navbar home

--- resources/views/showcart.blade.php
+++ resources/views/showcart.blade.php
@@ -11,7 +11,7 @@
 	<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&display=swap" rel="stylesheet">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-	<title>Klassy Cafe</title>
+	<title>ITALOT</title>
 
 	<!-- Additional CSS Files -->
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
@@ -19,6 +19,7 @@
 	<link rel="stylesheet" href="assets/css/templatemo-klassy-cafe.css">
 	<link rel="stylesheet" href="assets/css/owl-carousel.css">
 	<link rel="stylesheet" href="assets/css/lightbox.css">
+  <link rel="stylesheet" href="assets/css/header.css">
 	</head>
 	
 	<body>
@@ -33,87 +34,9 @@
     </div>  
     <!-- ***** Preloader End ***** -->
     
-    <!-- ***** Header Area Start ***** -->
-    <header class="header-area header-sticky">
-      <div class="container">
-        <div class="row">
-          <div class="col-12">
-            <nav class="main-nav">
-              <!-- ***** Logo Start ***** -->
-              <a href="{{url('/')}}" class="logo">
-                <img src="assets/images/klassy-logo.png" align="klassy cafe html template">
-              </a>
-              <!-- ***** Logo End ***** -->
-              <!-- ***** Menu Start ***** -->
-              <ul class="nav">
-                <li class="scroll-to-section"><a href="{{url('')}}" class="active">Home</a></li>
-                <li class="scroll-to-section"><a href="#about">About</a></li>
-                  
-              <!-- 
-                <li class="submenu">
-                  <a href="javascript:;">Drop Down</a>
-                  <ul>
-                    <li><a href="#">Drop Down Page 1</a></li>
-                    <li><a href="#">Drop Down Page 2</a></li>
-                    <li><a href="#">Drop Down Page 3</a></li>
-                  </ul>
-                </li>
-              -->
-              
-                <li class="scroll-to-section"><a href="#menu">Menu</a></li>
-                <li class="scroll-to-section"><a href="#chefs">Chefs</a></li> 
-                <li class="submenu">
-                  <a href="javascript:;">Features</a>
-                  <ul>
-                    <li><a href="#">Features Page 1</a></li>
-                    <li><a href="#">Features Page 2</a></li>
-                    <li><a href="#">Features Page 3</a></li>
-                    <li><a href="#">Features Page 4</a></li>
-                  </ul>
-                </li>
-                <!-- <li class=""><a rel="sponsored" href="https://templatemo.com" target="_blank">External URL</a></li> -->
-                <li class="scroll-to-section"><a href="#reservation">Contact Us</a></li>
-
-                <li class="scroll-to-section">
-
-                @auth 
-                <a href="{{url('/showcart', Auth::user()->id)}}">
-                Cart {{$count}}
-                </a>
-                @endauth
-                
-                @guest
-                  Cart [0]
-                @endguest
-                </li>
-
-                <li>
-                  @if (Route::has('login'))
-                    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                        @auth
-                          <li>
-                            <x-app-layout>
-                            </x-app-layout>
-                          </li>
-                        @else
-                          <li><a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a></li>
-                            @if (Route::has('register'))
-                              <li><a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a></li>
-                            @endif
-                        @endauth
-                    </div>
-                  @endif
-                </li>
-              </ul>
-              <a style="display: none;" class='menu-trigger'>
-                <span>Menu</span>
-              </a>       
-              <!-- ***** Menu End ***** -->
-            </nav>
-          </div>
-        </div>
-      </div>
-    </header>
+    <!-- ***** Header Area Start *****-->
+    @include('homenav')
+    <!-- ***** Header Area End ***** -->
 
     <div id="top">
       <table align="center" bgcolor="grey">
