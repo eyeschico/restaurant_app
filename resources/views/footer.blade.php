7f28828f9028f13df96f772b62d3ada8d32535c1
Mohammed Khalef <eyeschico@mohammedkhalef.com>
2021-10-21 18:27:57 +0200

    Add footer style

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,18 @@
+<footer>
+  <div class="footer-content">
+
+    <ul class="social-icons">
+      <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+      <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+      <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+      <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+    </ul>
+
+    <div class="uplogo">
+      <a class="italotfooter" href="{{url('/')}}">ITALOT</a>  
+    </div>
+  </div>
+
+
+
+</footer>
