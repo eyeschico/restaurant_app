f9ee4a59d198c26d89cc3761b37d0069bb278632
Mohammed Khalef <eyeschico@mohammedkhalef.com>
2021-10-21 17:25:50 +0200

    minor

--- resources/views/foodchef.blade.php
+++ resources/views/foodchef.blade.php
@@ -2,7 +2,7 @@
 <section class="section" id="chefs">
   <div class="container">
     <div class="row">
-      <div class="col-lg-4 offset-lg-4 text-center">
+      <div class="col-lg-4">
         <div class="section-heading">
           <h6>Our Chefs</h6>
           <h2>We offer the best ingredients for you</h2>
