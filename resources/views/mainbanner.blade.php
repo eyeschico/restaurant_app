421a17f921479e3ef22a62bc914101b3a4bbb64f
Mohammed Khalef <eyeschico@mohammedkhalef.com>
2021-10-29 20:04:50 +0200

    banner ok, minor

--- resources/views/mainbanner.blade.php
+++ resources/views/mainbanner.blade.php
@@ -1,38 +1,46 @@
 <div id="top">
-  <div class="container ">
-    <div class="">
-      <h6>Eat a lot with</h6>
-      <h4>ITALOT</h4>
-      
-      <div class="">
-        <a href="#reservation">Make A Reservation</a>
+  <div class="container">
+    <div class="banner">
+
+      <div class="left">
+        <div class="title">
+          <p>too fast,</p>
+          <p>too good like</p>
+          <h1>Boomy's</h1>
+        </div>
+          <div class="btn-reservation-main">
+            <a class="btn-reservation" href="#reservation">Make A Reservation</a>
+          </div>
+        
       </div>
+
+      <div class="right">
+        <div class="boomyscarousel">
+          <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img class="d-block w-100" src="/assets/images/slide-01.jpg" alt="First slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="/assets/images/slide-02.jpg" alt="Second slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="/assets/images/slide-03.jpg" alt="Third slide">
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+        </div>
+      </div>
+
     </div>
 
-    <div class="">
-      <div class="Modern-Slider">
-        <!-- Item -->
-        <div class="item">
-        <div class="img-fill">
-          <img src="assets/images/slide-01.jpg" alt="">
-        </div>
-        </div>
-        <!-- // Item -->
-        <!-- Item -->
-        <div class="item">
-        <div class="img-fill">
-          <img src="assets/images/slide-02.jpg" alt="">
-        </div>
-        </div>
-        <!-- // Item -->
-        <!-- Item -->
-        <div class="item">
-        <div class="img-fill">
-          <img src="assets/images/slide-03.jpg" alt="">
-        </div>
-        </div>
-        <!-- // Item -->
-      </div>
-    </div>
-
+  </div>
 </div>
