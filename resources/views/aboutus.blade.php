6275cc444e7be18a00684bb9d7c1827667312265
Mohammed Khalef <eyeschico@mohammedkhalef.com>
2021-10-29 15:28:53 +0200

    from scratch about us page

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -1,27 +1,23 @@
 <section class="section" id="about">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-6 col-md-6 col-xs-12">
-        <div class="left-text-content">
-          <div class="section-heading">
-            <h6>About Us</h6>
-            <h2>We Leave A Delicious Memory For You</h2>
-          </div>
-          <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore, culpa? Alias, consectetur quae soluta esse praesentium fuga dolorem aspernatur! Quis suscipit officiis eos magnam minus sapiente tempore sunt, dolore unde.
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est quis asperiores corporis necessitatibus quas id cum minus a porro, molestiae perferendis quibusdam voluptates? Esse rerum distinctio natus fugiat a commodi.
-          </p>
 
+    <div class="container">
+
+      <div class="">
+        <div class="">
+          <h6>About Us</h6>
+          <h2>We Leave A Delicious Memory For You</h2>
         </div>
+        <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore, culpa? Alias, consectetur quae soluta esse praesentium fuga dolorem aspernatur! Quis suscipit officiis eos magnam minus sapiente tempore sunt, dolore unde.
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est quis asperiores corporis necessitatibus quas id cum minus a porro, molestiae perferendis quibusdam voluptates? Esse rerum distinctio natus fugiat a commodi.
+        </p>
       </div>
-      <div class="col-lg-6 col-md-6 col-xs-12">
-        <div class="right-content">
-          <div class="thumb">
-            <input type="checkbox" id="btnControl"/>
-            <label class="btn" for="btnControl"><img id="scooter" src="assets/images/scooter-svgrepo-com.svg" alt="italot restaurant like"></label>
-          </div>
-        </div>
+
+      <div class="">
+        <input type="checkbox" id="btnControl"/>
+        <label class="btn" for="btnControl"><img id="scooter" src="assets/images/scooter-svgrepo-com.svg" alt="italot restaurant like"></label>
       </div>
+
     </div>
-  </div>
+
 </section>
