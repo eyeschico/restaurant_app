11def48c97414cbd40d4263ab8a58da0c851cf21
Mohammed Khalef <eyeschico@mohammedkhalef.com>
2021-10-30 21:29:08 +0200

    add favicon

--- resources/views/homehead.blade.php
+++ resources/views/homehead.blade.php
@@ -3,10 +3,11 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="Italote the best fictive fast food">
+  <meta name="description" content="Boomy's the best fictive fast food">
   <meta name="author" content="">
   <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&display=swap" rel="stylesheet">
+
 
   <title>Boomy's</title>
 
