e4663d93b4f2747974900cd7e4a3ae0abcbe3317
Mohammed Khalef <eyeschico@mohammedkhalef.com>
2021-10-30 15:32:58 +0200

    responsive about ok

--- resources/views/food.blade.php
+++ resources/views/food.blade.php
@@ -1,45 +1,42 @@
 <!-- ***** Menu Area Starts ***** -->
 <section class="section" id="menu">
   <div class="container">
-    <div class="row">
-      <div class="col-lg-4">
-        <div class="section-heading">
-          <h6>Our Menu</h6>
-          <h2>Our selection of cakes with quality taste</h2>
-        </div>
-      </div>
-    </div>
-  </div>
 
-  <div class="carte-flex">
+    <h6>Our Menu</h6>
+    <h2>Our selection of cakes with quality taste</h2>
 
+    <div class="carte-flex">
+      @foreach($data as $data)
+        <form action="{{url('/addcart', $data->id)}}" method="post">
+          @csrf
+            <div class='carte'>
 
-        @foreach($data as $data)
-          <form action="{{url('/addcart', $data->id)}}" method="post">
-            @csrf
-              <div class='carte'>
-
-                  <div class="carte-image" style="background-image: url('/foodimage/{{$data->image}}');" ></div>
-                  <div class="carte-text" height="20">
-                    <h2>{{$data->title}}</h2>
-                    <p>{{$data->description}}</p>
+                <div class="carte-image" style="background-image: url('/foodimage/{{$data->image}}');" ></div>
+                <div class="carte-text" height="20">
+                  <h2>{{$data->title}}</h2>
+                  <p>{{$data->description}}</p>
+                </div>
+                <div class="carte-adds">
+                  <div class="add">
+                    <h4 class="price">{{$data->price}}€</h4>
                   </div>
-                  <div class="carte-adds">
-                    <div class="add">
-                      <h4 class="price">{{$data->price}}€</h4>
-                    </div>
-                    <div class="add">
-                      <input class="quantity" type="number" name="quantity" min="1" value="0" >
-                    </div>
-                    <div class="add">
-                      <input class="submit" type="submit" value="Add cart">
-                    </div>
-                  </div> 
- 
-              </div>
-          </form>
-        @endforeach
+                  <div class="add">
+                    <input class="quantity" type="number" name="quantity" min="1" value="0" >
+                  </div>
+                  <div class="add">
+                    <input class="submit" type="submit" value="Add cart">
+                  </div>
+                </div> 
+
+            </div>
+        </form>
+      @endforeach
+    </div>
+
+
   </div>
+
+
 </section>
 <!-- ***** Menu Area Ends ***** -->
 
