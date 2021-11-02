7720db37ec3b057efd29d4a445be0993ccd9cf7f
Mohammed Khalef <eyeschico@mohammedkhalef.com>
2021-11-02 14:50:55 +0100

    add admin seeder

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      User::create([
+        'name' => 'admin',
+        'email' => 'admin@admin.com',
+        'usertype' => 1,
+        'password' => 'azerty12345',
+      ]);
+    }
+}
